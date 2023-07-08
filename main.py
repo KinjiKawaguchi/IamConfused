4d804f12c7d1ba87c48332df2d1855c6bc5f8471
河口 欣仁 <kawakin0310@icloud.com>
2023-07-08 18:29:46 +0900

    test update

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import streamlit as st
 import sqlite3
 from pages import student, teacher
 
-# Connect to SQLite database
+class User:
+    def __init__(self):
+        self.id = None
+        self.understanding = 5
+
+# Connect to SQLite database4
+user = User()
+
 conn = sqlite3.connect('confused.db')
 c = conn.cursor()
 
@@ -16,22 +23,39 @@ c.execute('''
 ''')
 conn.commit()
 
-# Authenticate user
-id = st.text_input('学籍番号を入力してください')
-password = st.text_input('誕生日を入力してください', type='password')
-if id and password:
-    c.execute('SELECT password FROM students WHERE id = ?', (id,))
-    result = c.fetchone()
-    if result is None:
-        # Register new user
-        c.execute('INSERT INTO students (id, password, understanding) VALUES (?, ?, NULL)', (id, password))
-        conn.commit()
-    elif result[0] != password:
-        st.error('誕生日が間違っています')
-        id = None  # Clear id to prevent access to pages
+while True:
+    # Authenticate user
+    id = st.text_input('学籍番号を入力してください')
+    password = st.text_input('誕生日を入力してください', type='password')
+    if id and password:
+        c.execute('SELECT password FROM students WHERE id = ?', (id,))
+        result = c.fetchone()
+        if result is None:
+            # Register new user
+            c.execute('INSERT INTO students (id, password, understanding) VALUES (?, ?, NULL)', (id, password))
+            user.id = id
+            conn.commit()
+            break
+        elif result[0] != password:
+            st.error('誕生日が間違っています')
+            id = None  # Clear id to prevent access to pages
+        else:
+            user.id = id
+            break
 
-# Page routing
-if id is not None:
-    student.show_page(conn, id)
-else:
-    teacher.show_page(conn)
+conn = sqlite3.connect('confused.db')
+c = conn.cursor()
+
+# Page for students
+understanding = st.radio(
+    '理解度を選択してください',
+    [('😭（全く理解できなかった）', 0), ('🥺（ほとんど理解できなかった）', 1), ('😨（大部分が理解できなかった）', 2),
+     ('😰（多少は理解できたがまだ難しい）', 3), ('😕（あまり理解できなかった）', 4), ('😐（半分くらい理解できた）', 5),
+     ('🙂（まあまあ理解できた）', 6), ('😊（ほぼ理解できた）', 7), ('😃（大部分を理解できた）', 8), ('😁（完全に理解できた）', 9)])
+
+if understanding is None:
+    understanding = 5
+# Update understanding in database
+c.execute('UPDATE students SET understanding = ? WHERE id = ?', (understanding, id))
+conn.commit()
+st.write('理解度が更新されました')
