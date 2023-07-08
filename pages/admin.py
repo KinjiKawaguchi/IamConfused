4d51d475cd0bef5ac3e07fc0b189d34e653b1c6b
河口 欣仁 <kawakin0310@icloud.com>
2023-07-08 19:13:27 +0900

    test update

--- pages/admin.py
+++ pages/admin.py
@@ -1,2 +1,24 @@
 import streamlit as st
 import sqlite3
+
+# Connect to SQLite database
+conn = sqlite3.connect('confused.db')
+c = conn.cursor()
+
+# Fetch all student data
+c.execute('SELECT * FROM students')
+students = c.fetchall()
+
+# Display all student data
+st.write('All student data:')
+for student in students:
+    st.write(student)
+
+# Allow admin to update understanding of a student
+st.write('Update student understanding:')
+id_to_update = st.text_input('Enter student ID')
+new_understanding = st.number_input('Enter new understanding level', min_value=0, max_value=9)
+if st.button('Update understanding'):
+    c.execute('UPDATE students SET understanding = ? WHERE id = ?', (new_understanding, id_to_update))
+    conn.commit()
+    st.write('Understanding updated.')
