da3e9be8be62a7ed27ab4817a1a091356eb0d6c8
河口 欣仁 <kawakin0310@icloud.com>
2023-07-08 17:26:04 +0900

    主にデータベーススキーマの更新と、IDとパスワードを使った生徒の認証機能の追加

--- main.py
+++ main.py
@@ -6,26 +6,34 @@ from pages import student, teacher
 conn = sqlite3.connect('confused.db')
 c = conn.cursor()
 
-# Create table for confused count if it doesn't exist
+# Create table for students if it doesn't exist
 c.execute('''
-    CREATE TABLE IF NOT EXISTS confused_count (
-        id INTEGER PRIMARY KEY,
-        count INTEGER
+    CREATE TABLE IF NOT EXISTS students (
+        id TEXT PRIMARY KEY,
+        password TEXT,
+        understanding INTEGER
     )
 ''')
-
-# Initialize count in database if it doesn't exist
-c.execute('''
-    INSERT INTO confused_count (id, count)
-    SELECT 1, 0
-    WHERE NOT EXISTS(SELECT 1 FROM confused_count WHERE id = 1)
-''')
 conn.commit()
+
+# Authenticate user
+id = st.sidebar.text_input('学籍番号を入力してください')
+password = st.sidebar.text_input('誕生日を入力してください', type='password')
+if id and password:
+    c.execute('SELECT password FROM students WHERE id = ?', (id,))
+    result = c.fetchone()
+    if result is None:
+        # Register new user
+        c.execute('INSERT INTO students (id, password, understanding) VALUES (?, ?, NULL)', (id, password))
+        conn.commit()
+    elif result[0] != password:
+        st.sidebar.error('誕生日が間違っています')
+        id = None  # Clear id to prevent access to pages
 
 # Page routing
 page = st.sidebar.selectbox('ページを選択', ['生徒向けページ', '先生向けページ'])
 
-if page == '生徒向けページ':
-    student.show_page(conn)
+if page == '生徒向けページ' and id is not None:
+    student.show_page(conn, id)
 elif page == '先生向けページ':
     teacher.show_page(conn)
