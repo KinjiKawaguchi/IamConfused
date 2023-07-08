86adbbbc9ccaaefb976e9e81a1f1e70506b54f42
河口 欣仁 <kawakin0310@icloud.com>
2023-07-08 17:26:04 +0900

    update

--- pages/teacher.py
+++ pages/teacher.py
@@ -1,11 +1,22 @@
 import streamlit as st
 import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def show_page(conn):
     c = conn.cursor()
 
     # Page for teacher
-    # Get count from database
-    c.execute('SELECT count FROM confused_count WHERE id = 1')
-    count = c.fetchone()[0]
-    st.write('現在のわからない人の数は {} 人です。'.format(count))
+    # Get understanding counts from database
+    c.execute('SELECT understanding, COUNT(*) FROM students WHERE understanding IS NOT NULL GROUP BY understanding')
+    results = c.fetchall()
+
+    # Show as a bar chart
+    df = pd.DataFrame(results, columns=['Understanding', 'Count'])
+    df = df.sort_values('Understanding')
+    plt.bar(df['Understanding'], df['Count'], tick_label=df['Understanding'])
+    plt.xlabel('Understanding')
+    plt.ylabel('Count')
+    plt.title('Understanding Distribution')
+    st.pyplot(plt.gcf())
+    plt.clf()
