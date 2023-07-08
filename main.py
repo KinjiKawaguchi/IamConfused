9324448e1fbd869f6a0fd04e2746ebfdac1de27e
河口 欣仁 <kawakin0310@icloud.com>
2023-07-08 17:43:47 +0900

    update

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ c.execute('''
 conn.commit()
 
 # Authenticate user
-id = st.sidebar.text_input('学籍番号を入力してください')
-password = st.sidebar.text_input('誕生日を入力してください', type='password')
+id = st.text_input('学籍番号を入力してください')
+password = st.text_input('誕生日を入力してください', type='password')
 if id and password:
     c.execute('SELECT password FROM students WHERE id = ?', (id,))
     result = c.fetchone()
@@ -27,13 +27,11 @@ if id and password:
         c.execute('INSERT INTO students (id, password, understanding) VALUES (?, ?, NULL)', (id, password))
         conn.commit()
     elif result[0] != password:
-        st.sidebar.error('誕生日が間違っています')
+        st.error('誕生日が間違っています')
         id = None  # Clear id to prevent access to pages
 
 # Page routing
-page = st.sidebar.selectbox('ページを選択', ['生徒向けページ', '先生向けページ'])
-
-if page == '生徒向けページ' and id is not None:
+if id is not None:
     student.show_page(conn, id)
-elif page == '先生向けページ':
+else:
     teacher.show_page(conn)
