bbe96eae36261505ee081baf34b1879ed7c7571e
河口 欣仁 <kawakin0310@icloud.com>
2023-07-08 17:50:47 +0900

    test update

--- pages/teacher.py
+++ pages/teacher.py
@@ -3,20 +3,20 @@ import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def show_page(conn):
-    c = conn.cursor()
+conn = sqlite3.connect('confused.db')
+c = conn.cursor()
 
-    # Page for teacher
-    # Get understanding counts from database
-    c.execute('SELECT understanding, COUNT(*) FROM students WHERE understanding IS NOT NULL GROUP BY understanding')
-    results = c.fetchall()
+# Page for teacher
+# Get understanding counts from database
+c.execute('SELECT understanding, COUNT(*) FROM students WHERE understanding IS NOT NULL GROUP BY understanding')
+results = c.fetchall()
 
-    # Show as a bar chart
-    df = pd.DataFrame(results, columns=['Understanding', 'Count'])
-    df = df.sort_values('Understanding')
-    plt.bar(df['Understanding'], df['Count'], tick_label=df['Understanding'])
-    plt.xlabel('Understanding')
-    plt.ylabel('Count')
-    plt.title('Understanding Distribution')
-    st.pyplot(plt.gcf())
-    plt.clf()
+# Show as a bar chart
+df = pd.DataFrame(results, columns=['Understanding', 'Count'])
+df = df.sort_values('Understanding')
+plt.bar(df['Understanding'], df['Count'], tick_label=df['Understanding'])
+plt.xlabel('Understanding')
+plt.ylabel('Count')
+plt.title('Understanding Distribution')
+st.pyplot(plt.gcf())
+plt.clf()
