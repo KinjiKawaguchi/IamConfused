29c264da366e999188f6826c5766dc8dbb8e99b0
KinjiKawaguchi <93261102+KinjiKawaguchi@users.noreply.github.com>
2023-11-06 17:27:30 +0900

    Update confused.py

--- confused.py
+++ confused.py
@@ -1,47 +1,32 @@
 import streamlit as st
 
-# セッションステートで現在の金額を追跡します
+# 状態遷移の辞書
+transitions = {
+    2000: {'払う': 3000, '払えない': 1000},
+    3000: {'払う': 4000, '払えない': '終了 (2000-3000)'},
+    4000: {'払う': '終了 (4000-)', '払えない': '終了 (3000-4000)'},
+    1000: {'払う': '終了 (1000-2000)', '払えない': '終了 (-1000)'}
+}
+
+# セッションステートの初期化
 if 'current_offer' not in st.session_state:
-    st.session_state['current_offer'] = 2000  # 初期オファー
+    st.session_state['current_offer'] = 2000
 
-# ユーザーが「払える」または「払えない」と答えた場合のロジックを関数にまとめます
+# ユーザーのレスポンスに応じた処理を行う関数
 def handle_response(response):
-    current_offer = st.session_state['current_offer']
-    
-    # ユーザーの応答に基づいて次の状態に遷移します
-    if current_offer == 2000:
-        if response == '払う':
-            st.session_state['current_offer'] = 3000
-        else:  # '払えない'
-            st.session_state['current_offer'] = 1000
-    
-    elif current_offer == 1000:
-        if response == '払える':
-            st.success('終了 (1000-2000)')
-            st.session_state['current_offer'] = '終了'
-        else:  # '払えない'
-            st.success('終了 (-1000)')
-            st.session_state['current_offer'] = '終了'
-    
-    elif current_offer == 3000:
-        if response == '払えない':
-            st.success('終了 (2000-3000)')
-            st.session_state['current_offer'] = '終了'
-        else:  # '払える'
-            st.session_state['current_offer'] = 4000
-    
-    elif current_offer == 4000:
-        if response == '払えない':
-            st.success('終了 (3000-4000)')
-            st.session_state['current_offer'] = '終了'
-        else:  # '払える'
-            st.success('終了 (4000-)')
+    if st.session_state['current_offer'] in transitions:
+        next_state = transitions[st.session_state['current_offer']][response]
+        
+        if isinstance(next_state, int):  # まだ終了していない場合
+            st.session_state['current_offer'] = next_state
+        else:  # 終了する場合
+            st.success(next_state)
             st.session_state['current_offer'] = '終了'
 
-# アンケートの質問と応答ボタンを表示します
-if st.session_state['current_offer'] != '終了':
+# アンケートの質問と応答ボタンを表示
+if isinstance(st.session_state['current_offer'], int):
     st.write(f"あなたは {st.session_state['current_offer']} 円を支払う意思がありますか？")
-    
+
     if st.button('払える'):
         handle_response('払う')
     elif st.button('払えない'):
