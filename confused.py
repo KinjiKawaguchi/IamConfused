37b43559b84f8a7729ba9d8a12d79110e1825838
KinjiKawaguchi <93261102+KinjiKawaguchi@users.noreply.github.com>
2023-11-06 17:43:41 +0900

    Update confused.py

--- confused.py
+++ confused.py
@@ -12,20 +12,20 @@ def is_ended(offer):
 def update_offer(agree):
     if agree:
         # 払うことに同意した場合
-        if st.session_state.current_offer == 2000:
-            st.session_state.current_offer = 3000
-        elif st.session_state.current_offer == 3000:
-            st.session_state.current_offer = 4000
-        elif st.session_state.current_offer == 4000:
-            st.session_state.current_offer = '終了 (4000以上)'
+        if st.session_state['current_offer'] == 2000:
+            st.session_state['current_offer'] = 3000
+        elif st.session_state['current_offer'] == 3000:
+            st.session_state['current_offer'] = 4000
+        elif st.session_state['current_offer'] == 4000:
+            st.session_state['current_offer'] = '終了 (4000以上)'
     else:
         # 払うことに同意しなかった場合
-        if st.session_state.current_offer == 2000:
-            st.session_state.current_offer = 1000
-        elif st.session_state.current_offer == 3000:
-            st.session_state.current_offer = '終了 (2000-3000)'
-        elif st.session_state.current_offer == 1000:
-            st.session_state.current_offer = '終了 (1000以下)'
+        if st.session_state['current_offer'] == 2000:
+            st.session_state['current_offer'] = 1000
+        elif st.session_state['current_offer'] == 3000:
+            st.session_state['current_offer'] = '終了 (2000-3000)'
+        elif st.session_state['current_offer'] == 1000:
+            st.session_state['current_offer'] = '終了 (1000以下)'
 
 # メインアプリケーション
 def main():
@@ -34,15 +34,18 @@ def main():
     else:
         st.write(f"あなたは {st.session_state['current_offer']} 円を支払う意思がありますか？")
 
+        agree_key = f"agree_{st.session_state['current_offer']}"
+        disagree_key = f"disagree_{st.session_state['current_offer']}"
+
         col1, col2 = st.columns(2)
         with col1:
-            if st.button('払える'):
+            if st.button('払える', key=agree_key):
                 update_offer(True)
         with col2:
-            if st.button('払えない'):
+            if st.button('払えない', key=disagree_key):
                 update_offer(False)
 
         # 再描画を避けるために状態の変更後に即時アップデートする
-        main()
+        st.experimental_rerun()
 
 main()
