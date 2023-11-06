6d3b07b3fdcc4d64890bd8b1ca913cdc06f0c183
KinjiKawaguchi <93261102+KinjiKawaguchi@users.noreply.github.com>
2023-11-06 18:06:31 +0900

    Update confused.py

--- confused.py
+++ confused.py
@@ -6,7 +6,8 @@ db = DatabaseManager('wtp.db')
 db.create_tables_if_not_exists()
 
 # ユーザー認証のフラグ
-login = False
+if 'login' not in st.session_state:
+    st.session_state.login = False
 
 # ユーザーIDとパスワード入力フィールド
 id = st.text_input('学籍番号を入力してください')
@@ -18,20 +19,19 @@ if id and password:
     if result is None:
         # 新規ユーザー登録
         db.register_student(id, password)
-        login = True
+        st.session_state.login = True
     elif result[0] != password:
         st.error('誕生日が間違っています')
-        login = False
     else:
         st.success("ログインに成功しました。")
-        login = True
+        st.session_state.login = True
 
 # ログインしていなければ指示を出力
-if not login:
+if not st.session_state.login:
     st.warning("学籍番号と誕生日を入力してください。")
 
 # WTP質問フロー
-if login:
+if st.session_state.login:
     # 状態遷移の辞書
     state_transitions = {
         2000: {'払う': 3000, '払えない': 1000},
@@ -40,25 +40,30 @@ if login:
         1000: {'払える': '終了(1000-2000)', '払えない': '終了(-1000)'}
     }
 
-    # ユーザーの現在の状態
-    current_state = 2000  # 初期状態
+    # 初期状態をセットする
+    if 'current_state' not in st.session_state:
+        st.session_state.current_state = 2000
 
-    # 状態とボタンを表示
-    st.write(f"現在の価格: ¥{current_state}")
+    # 現在の状態を表示
+    st.write(f"現在の価格: ¥{st.session_state.current_state}")
+
+    # ボタンを表示し、遷移ロジックを実装
     col1, col2 = st.columns(2)
     with col1:
         if st.button('払える'):
-            next_state = state_transitions[current_state].get('払える')
+            next_state = state_transitions[st.session_state.current_state].get('払える')
+            if isinstance(next_state, str) and next_state.startswith('終了'):
+                st.success(f"あなたのWTPは {next_state} です。")
+                # データベースにWTPを記録する
+                db.record_wtp(id, st.session_state.current_state, next_state)
+            else:
+                st.session_state.current_state = next_state
+                st.experimental_rerun()
+
     with col2:
         if st.button('払えない'):
-            next_state = state_transitions[current_state].get('払えない')
-
-    # 次の状態が終了状態ならばデータベースに記録して結果を表示
-    if isinstance(next_state, str) and next_state.startswith('終了'):
-        st.success(f"あなたのWTPは {next_state} です。")
-        # データベースにWTPを記録するコードをここに書く
-        # 例: db.record_wtp(id, current_state, next_state)
-    elif next_state:
-        # 状態を更新して再度表示
-        current_state = next_state
-        st.experimental_rerun()
+            next_state = state_transitions[st.session_state.current_state].get('払えない')
+            if isinstance(next_state, str) and next_state.startswith('終了'):
+                st.success(f"あなたのWTPは {next_state} です。")
+                # データベースにWTPを記録する
+                db.record_wtp(id, st.session_state.current_state,
